ab6f2e0a3eb40c608b86a27d8b168996b9135770
Junseong Lee <108290934+junjslee@users.noreply.github.com>
2025-03-20 05:13:45 -0500

    Delete losses.py

--- losses.py
+++
@@ -1,9 +0,0 @@
-import torch.nn as nn
-
-class ClassificationLoss(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.loss_fn = nn.BCEWithLogitsLoss()
-    
-    def forward(self, logits, targets):
-        return self.loss_fn(logits, targets)
